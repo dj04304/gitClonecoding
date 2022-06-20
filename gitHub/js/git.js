04fb8f0c5bb59ca19e8259a8749ebcfacf44134f
dj04304 <dj043045@gmail.com>
2022-06-21 08:56:58 +0900

    220621 gitHub repository 생성 클론코딩 수정 (span > underline js 및 menu mouseover mouseout js 추가)

--- gitHub/js/git.js
+++ gitHub/js/git.js
@@ -1,1 +1,48 @@
-const span = document.querySelector(".import");
+// menu에 hover 했을 때 텍스트 색의 변화
+const menu = document.querySelectorAll(".menu_over");
+// span_tag 에 밑줄
+const span = document.querySelectorAll(".span_tag");
+// select에 hover 했을 때 배경 색의 변화
+const select = document.querySelectorAll("select");
+console.log(select)
+
+// span 반복문
+for(let i = 0; i < span.length; i++){
+    span[i].onmouseover = () => {
+        span[i].classList.toggle("line");
+    }
+}
+
+for(let i = 0; i < span.length; i++){
+    span[i].onmouseout = () => {
+        span[i].classList.toggle("line");
+    }
+}
+
+// menu 반복문
+for(let i = 0; i < menu.length; i++){
+    menu[i].onmouseover = () => {
+        menu[i].classList.toggle("menu_over_out");
+    }
+}
+
+for(let i = 0; i < menu.length; i++){
+    menu[i].onmouseout = () => {
+        menu[i].classList.toggle("menu_over_out");
+    }
+}
+
+// select 반복문
+for(let i = 0; i < select.length; i++){
+    select[i].onmouseover = () => {
+        select[i].classList.toggle("selected");
+    }
+}
+
+for(let i = 0; i < select.length; i++){
+    select[i].onmouseout = () => {
+        select[i].classList.toggle("selected");
+    }
+}
+
+
